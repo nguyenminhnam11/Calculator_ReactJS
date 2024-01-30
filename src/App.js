df0be5f64db370446acf843bca41c3020212fe66
Nam Nguyen <101324693+nguyenminhnam11@users.noreply.github.com>
2024-01-30 14:33:39 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (event) => {
+    setResult(result.concat(event.target.value));
+  };
+
+  const clear = () => {
+    setResult("")
+  }
+
+  const del = () => {
+    setResult(result.slice(0, - 1))
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString())
+    } catch(error) {
+      setResult("Error")
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <input className="previous-operand" type="text" value={result} />
+      </div>
+      <button className="span-two" onClick={clear}>AC</button>
+      <button onClick={del} className="highlight">DEL</button>
+      <button value="/" onClick={handleClick} className="highlight">÷</button>
+      <button value="1" onClick={handleClick}>1</button>
+      <button value="2" onClick={handleClick}>2</button>
+      <button value="3" onClick={handleClick}>3</button>
+      <button value="*" onClick={handleClick} className="highlight">*</button>
+      <button value="4" onClick={handleClick}>4</button>
+      <button value="5" onClick={handleClick}>5</button>
+      <button value="6" onClick={handleClick}>6</button>
+      <button value="+" onClick={handleClick} className="highlight">+</button>
+      <button value="7" onClick={handleClick}>7</button>
+      <button value="8" onClick={handleClick}>8</button>
+      <button value="9" onClick={handleClick}>9</button>
+      <button value="-" onClick={handleClick} className="highlight">-</button>
+      <button value="." onClick={handleClick}>.</button>
+      <button value="0" onClick={handleClick}>0</button>
+      <button className="span-two" onClick={calculate}>=</button>
     </div>
   );
 }
